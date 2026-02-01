86107c90c292f52a8b42f0df896ed0c748038bb9
Namballa-Srivansh <srivansh90@gmail.com>
2026-02-01 17:51:04 +0530

    Dynamic Routing

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,12 @@
+// Dynamic Routing
+
+const UserDetails = async ({params}: {params: Promise<{id: string}>}) => {
+    const {id} = await params
+    return (
+        <div>
+            <h1>Showing details for user #{id}</h1>
+        </div>
+    )
+}
+
+export default UserDetails
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,21 @@
+// Dynamic Routing
+
+import Link from "next/link"
+
+const Users = () => {
+  return ( 
+    <>
+        <h1>Dashboard Users</h1>
+
+        <ul className="mt-10">
+            <li><Link href="/dashboard/users/1">User1</Link></li>
+            <li><Link href="/dashboard/users/2">User2</Link></li>
+            <li><Link href="/dashboard/users/3">User3</Link></li>
+            <li><Link href="/dashboard/users/4">User4</Link></li>
+            <li><Link href="/dashboard/users/5">User5</Link></li>
+        </ul>
+    </>
+  )
+}
+
+export default Users
